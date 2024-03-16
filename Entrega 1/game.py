245706b83e118bee898cbba3fe1108dd6f82905f
francisco ronga <franciscoronga00@gmail.com>
2024-03-16 19:05:50 -0300

    Mejora en la forma de elegir nivel

--- Entrega 1/game.py
+++ Entrega 1/game.py
@@ -15,27 +15,33 @@ print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 
 #Cambio la word_displayed dependiendo el nivel seleccionado
-posibles_niveles={"Facil","Media","Dificil"}
-nivel=input(f"En que nivel desea jugar{posibles_niveles}")
+posibles_niveles={"1":"Facil","2":"Media","3":"Dificil"}
+print(">En que nivel desea jugar:")
+for i, j in posibles_niveles.items():
+    print(f"{' ' * 4} {i} - {j}")
 
-#Por si hay un error de tipeo cuando se ingresa el nivel
-while nivel not in posibles_niveles:
-    nivel=(f"Nivel ingresado no disponible, vuelva a elegir {posibles_niveles}")
+while True:
+    nivel=input(f">Elegir un nivel: ")
+    if nivel not in posibles_niveles:
+        print(f"!Nivel ingresado inexistente, vuelva a elegir ")
+    else:
+        nivel=int(nivel)
+        break
 
 word_displayed=""
-if nivel == "Facil": #Se muestran todas las vocales
+if nivel == 1: #Se muestran todas las vocales
     for i in secret_word:
-        if i in "aeiou":
+        if i in "aeiouáéíóú":
             word_displayed+=i
             guessed_letters.append(i)
         else:
             word_displayed+="_"
-elif nivel == "Media":  #Se muestra la primer y ultima letra
+elif nivel == 2:  #Se muestra la primer y ultima letra
 
     """Como el ejercicio no especifica se podía hacer >word_displayed = (secret_word[0]+ "-" * (len((secret_word))-2) + secret_word[(len((secret_word))-1)])<
     Pero en caso de que alguna de estas dos letras vuelva a aparecer en el medio de la palabra, quedaba sin mostrarse y generaba confusion,
     por lo tanto decidi mostrar la letra en caso de repetición."""
-    
+    #Queda por hacer el caso de que haya alguna vocal en las puntas y en el centro esté con acento.
     guessed_letters.append(secret_word[0])
     guessed_letters.append(secret_word[(len((secret_word))-1)])
     for i in secret_word:
@@ -43,27 +49,27 @@ elif nivel == "Media":  #Se muestra la primer y ultima letra
             word_displayed+=i
         else:
             word_displayed+="_"
-elif nivel == "Dificil": #No se muestra ninguna letra de la palabra
+elif nivel == 3: #No se muestra ninguna letra de la palabra
     word_displayed = "_" * len(secret_word)
 
 # Mostrarla palabra parcialmente adivinada
-print(f"Palabra: {word_displayed}")
+print(f"/Palabra: {word_displayed}")
 
 #Puede fallar max_attempts veces
 while failed_attempts < max_attempts:
     # Pedir al jugador que ingrese una letra
-    letter = input("Ingresa una letra: ").lower()
+    letter = input(">Ingresa una letra: ").lower()
     #Checkeo que el input sea UNA sola letra, y parte del abecedario.
     if not letter.isalpha() or len(letter)!=1:
-        print("Ingresar una letra del abecedario.")
+        print("!Ingresar una letra del abecedario.")
         continue
     #Verifico si el input fue "", se cuenta como error y pierde intento
     if letter == "":
-        print("Error no se introdujo ninguna letra, vuelva a intentarlo")
+        print("!Error no se introdujo ninguna letra, vuelva a intentarlo")
         continue
     # Verificar si la letra ya ha sido adivinada
     if letter in guessed_letters:
-        print("Ya has intentado con esa letra. Intenta con otra.")
+        print("!Ya has intentado con esa letra. Intenta con otra.")
         continue
 
     # Agregar la letra a la lista de letras adivinadas
