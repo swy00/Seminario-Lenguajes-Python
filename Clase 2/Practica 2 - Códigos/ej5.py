4fe9601e5a71ead59a1e8cae86e3eed68c352dfc
francisco ronga <franciscoronga00@gmail.com>
2024-04-06 03:55:56 -0300

    Ejercicio 5

--- Clase 2/Practica 2 - Códigos/ej5.py
+++ Clase 2/Practica 2 - Códigos/ej5.py
@@ -10,8 +10,34 @@ distributed ABM toolkit. We build on our experiences in developing ABM
 toolkits, including Repast for High Performance Computing (Repast 
 HPC), to identify the key elements of a useful distributed ABM 
 toolkit. We leverage the Numba, NumPy, and PyTorch packages and the 
-En este ejemplo se debe informar:
 Python C-API to create a scalable modeling system that can exploit the
 largest HPC resources and emerging computing architectures. """
 
+#Encuentro el inicio y fin, ignoro la palabra clave del principio con el "+len(asd)"" y con .strip elimino los espacios vacios
+titulo = article[(article.find("título:")+ len("título:")):(article.find("resumen:"))].strip()
+resumen = article[(article.find("resumen:")+ len("resumen:")):(len(article))].strip()
 
+#Como los numeros no cuentan, los elimino de los strings
+titulo = ''.join(i for i in titulo if not i.isdigit())
+resumen = ''.join(i for i in resumen if not i.isdigit())
+
+facil=0
+aceptable=0
+dificil=0
+muy_dificil =0
+informe_titulo = "not ok" if len(titulo.split()) > 10 else "ok"
+
+#Recorro oracion por oracion y veo si cumple las condiciones
+oraciones = resumen.split(".")
+for i in oraciones:
+    palabras_oracion=i.split()
+    if len(palabras_oracion) <= 12:
+        facil+=1
+    elif 13 <= len(palabras_oracion) <= 17:
+        aceptable+=1
+    elif 18 <= len(palabras_oracion) <= 25:
+        dificil += 1
+    else:
+        muy_dificil += 1
+
+print(f"titulo: {informe_titulo}\nCantidad de oraciones faciles de leer: {facil}, aceptables para leer:{aceptable},dificil de leer: {dificil}, muy dificil de leer {muy_dificil}.")
